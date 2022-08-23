170102437694f9c074d4c1f28d7ac20852d84d2b
johannamatero <johanna.matero@hotmail.com>
2022-08-23 19:15:01 +0200

    Added update_server functionality

--- baseutils/urls.py
+++ baseutils/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='baseutils')
+    path('', views.index, name='baseutils'),
+    path('update_server/', views.update, name='update')
 ]
--- baseutils/views.py
+++ baseutils/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+import git
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
     return render(request, 'baseutils/base.html')
+
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        '''
+        pass the path of the diectory where your project will be 
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "test.pythonanywhere.com"
+        '''
+        repo = git.Repo("beslutsstod.pythonanywhere.com/") 
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
