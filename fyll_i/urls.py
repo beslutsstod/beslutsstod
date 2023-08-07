ecd7cb5dbd260520478c6d4062b8e0f111e219f5
jmatero <johanna.matero@cubist.eu>
2023-08-07 14:30:20 +0200

    Add Intervention view

--- fyll_i/urls.py
+++ fyll_i/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='fyll-i')
+    path('', views.index, name='fyll-i'),
+    path('/intervention', views.intervention, name='fyll-i/intervention'),
+    path('/uppna', views.uppna, name='fyll-i/uppna')
 ]
--- fyll_i/views.py
+++ fyll_i/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'fyll_i/fyll_i.html')
+
+def intervention(request):
+    return render(request, 'fyll_i/intervention.html')
+
+def uppna(request):
+    return render(request, 'fyll_i/uppna.html')
